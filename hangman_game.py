7b99841dce87718915e75c0bd15d91a2b0128208
arsan-che <arsan903@gmail.com>
2025-02-17 22:29:10 +0000

    created Hangman class with four parameters and six arguments

---
+++ hangman_game.py
@@ -0,0 +1,11 @@
+import random
+
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5, **kwargs): 
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = [] 

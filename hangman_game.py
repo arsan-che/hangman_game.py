08b75cf7212f2bef072ec84a38c2d66dcf161f65
arsan-che <arsan903@gmail.com>
2025-02-18 14:33:06 +0000

    Added check guess function that insuress lower case input. And print two statements based on if the letter is correct or wrong.

--- hangman_game.py
+++ hangman_game.py
@@ -10,3 +10,16 @@ class Hangman:
         self.word_list = word_list
         self.list_of_guesses = [] 
         
+    def check_guess(self, guess):
+      guess = guess.lower()
+      if guess in self.word:
+        print(f"Good guess! {guess} is in the word.")
+        for i, letter in enumerate(self.word):
+          if letter == guess:
+            self.word_guessed[i] = guess
+            self.num_letters -= 1
+        print(self.word_guessed)
+      else:
+          print(f"Sorry, {guess} is not in the word.")
+          self.num_lives -= 1
+          print(f"You have {self.num_lives} lives left.")
